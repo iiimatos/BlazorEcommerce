a4ebd86516fa65495a35a5773653fccda0a1eff9
iiimatos <ivanmatosvillar@gmail.com>
2022-02-22 02:29:19 -0400

    Section 3 - Finished

---
+++ BlazorEcommerce/Server/Services/ProductService/IProductService.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorEcommerce.Server.Services.ProductService
+{
+    public interface IProductService
+    {
+       // Task<ServicesResponse<Product>> GetProductByIdAsync(int id);
+        Task<ServicesResponse<List<Product>>> GetProductsAsync();
+
+    }
+}
---
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorEcommerce.Server.Services.ProductService
+{
+    public class ProductService : IProductService
+    {
+        private readonly DataDbContext _context;
+
+        public ProductService(DataDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ServicesResponse<List<Product>>> GetProductsAsync()
+        {
+            var response = new ServicesResponse<List<Product>>()
+            {
+                Data = await _context.Products.ToListAsync()
+            };
+
+            return response;
+        }
+    }
+}
